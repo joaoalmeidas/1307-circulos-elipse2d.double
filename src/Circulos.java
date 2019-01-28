7c263ecf13df2b11d41fd3570f7b8d67c0e7698b
joao <joao182cx@gmail.com>
2019-01-28 16:01:29 -0200

    Modificação do modo de desenho dos círculos de g.drawOval() para g2d.draw(Ellipse.Double()).

--- src/Circulos.java
+++ src/Circulos.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 import java.security.SecureRandom;
 
 import javax.swing.JPanel;
@@ -10,21 +12,22 @@ public class Circulos extends JPanel{
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
 		
 		final int centroY = getHeight()/2 - 5;
 		final int centroX = getWidth()/2 - 5;
 		final SecureRandom aleatorio = new SecureRandom();
 		
-		g.setColor(Color.RED);
-		g.drawLine(centroX + 10, centroY + 5, centroX + 20, centroY + 5);
+		g2d.setColor(Color.RED);
+		g2d.drawLine(centroX + 10, centroY + 5, centroX + 20, centroY + 5);
 		
 
 		
 		for(int i = 0; i < 100; i+=10) {
 			
-			g.setColor(new Color(aleatorio.nextInt(256), aleatorio.nextInt(256), aleatorio.nextInt(256)));
+			g2d.setColor(new Color(aleatorio.nextInt(256), aleatorio.nextInt(256), aleatorio.nextInt(256)));
 			
-			g.drawArc(centroX - i, centroY - i, 10 + i * 2, 10 + i * 2, 0, 360);
+			g2d.draw(new Ellipse2D.Double(centroX - i, centroY - i, 10 + i * 2, 10 + i * 2));
 			
 		}
 		
